e1c8cbb85789e5bf3bbe512b0bf143be5ca37870
masoud <masoud.gh20@gmail.com>
2023-11-16 22:08:10 +0330

    models added to app

--- booking_project/booking/admin.py
+++ booking_project/booking/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hotel, Room, Booking
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "created_at",
+    )
+    fields = ("id", "name", "created_at", "updated_at")
+    readonly_fields = ("id", "created_at", "updated_at")
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "hotel",
+        "name",
+        "created_at",
+    )
+    list_filter = ("hotel__name",)
+    fields = ("id", "hotel", "name", "created_at", "updated_at")
+    readonly_fields = ("id", "created_at", "updated_at")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "room",
+        "created_at",
+    )
+    list_filter = ("room__hotel__name",)
+    fields = ("id", "username", "room", "created_at", "updated_at")
+    readonly_fields = ("id", "created_at", "updated_at")
--- booking_project/booking/models.py
+++ booking_project/booking/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Hotel(TimeStampedModel):
+    name = models.CharField(max_length=32)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Room(TimeStampedModel):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=16)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Booking(TimeStampedModel):
+    username = models.CharField(max_length=32)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    checkin = models.DateField()
+    checkout = models.DateField()
+
+    def __str__(self) -> str:
+        return self.id
